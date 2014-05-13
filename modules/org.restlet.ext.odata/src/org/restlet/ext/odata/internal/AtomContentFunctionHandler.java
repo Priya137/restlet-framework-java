9ab13a068a69f8a4cfbd413838b24138b92ae5cf
Onkar Dhuri <onkar.dhuri@gmail.com>
2014-05-13 19:18:29 +0530

    Code Cleanup : Removed unused code and imports

--- modules/org.restlet.ext.odata/src/org/restlet/ext/odata/internal/AtomContentFunctionHandler.java
+++ modules/org.restlet.ext.odata/src/org/restlet/ext/odata/internal/AtomContentFunctionHandler.java
@@ -27,7 +27,6 @@ public class AtomContentFunctionHandler extends XmlFormatParser implements Funct
 	 * java.lang.String, java.util.List)
 	 */
 	@SuppressWarnings({ "unused", "unchecked", "rawtypes" })
-	@Override
 	public Object parseResult(Class<?> classType,
 			Representation representation, String functionName, List<?> entity) {
 		try {
